bf5a3e26f2fadc4e8c212e24ab1af21eec791aa4
lisatomelin <lisa.tomelin@gmail.com>
2024-04-23 09:48:04 -0300

    :zap: add lazy loading

---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
+import { RouterModule } from '@angular/router';
+import { DASHBOARD_ROUTES } from './page/dashboard.routing';
+import { ToastModule } from 'primeng/toast';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar'
+import { MessageService } from 'primeng/api';
+import { SidebarModule } from 'primeng/sidebar';
+import { CookieService } from 'ngx-cookie-service';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(DASHBOARD_ROUTES),
+    ToastModule,
+    CardModule,
+    ButtonModule,
+    ToolbarModule, 
+    SidebarModule,
+  ],
+  providers: [MessageService, CookieService],
+})
+export class DashboardModule { }
